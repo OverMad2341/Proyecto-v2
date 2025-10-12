a45716dc4ec3738be2992e4312d236373b1e38dd
multiplayersw <isaacj.silvaz39@gmail.com>
2025-10-11 23:52:33 -0400

    migraciones y Hcaptcha

---
+++ database/migrations/2025_10_12_033856_movement_history_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('movement_history', function (Blueprint $table) {
+            $table->id();
+            
+            $table->unsignedBigInteger('id_bien');
+            $table->unsignedBigInteger('id_organizational_uni');
+            $table->unsignedBigInteger('id_made_by');
+
+
+            $table->foreign('id_bien')
+                ->references('id_bien')
+                ->on('national_asset')
+                ->onDelete('restrict');
+            
+            $table->foreign('id_organizational_uni')
+                ->references('id')
+                ->on('organizational_uni')
+                ->onDelete('restrict');
+
+            $table->foreign('id_made_by')
+                ->references('id')
+                ->on('employee')
+                ->onDelete('restrict');
+
+            $table->timestamp('movement_date');
+            $table->text('reason')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('movement_history');
+    }
+};
