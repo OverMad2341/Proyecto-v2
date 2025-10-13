c829d79558350b93419af2f18bb2ee7372b744bc
anth0025 <lathoagl@gmail.com>
2025-10-12 23:02:07 -0400

    archivo seeder y instalacion de backpack/crud

--- database/migrations/2025_08_16_213121_create_national_asset_assignament_table.php
+++ database/migrations/2025_08_16_213121_create_national_asset_assignament_table.php
@@ -31,7 +31,7 @@ return new class extends Migration
                 ->references('id')
                 ->on('employee')
                 ->onDelete('restrict');
-           
+        
         });
     }
 
---
+++ database/seeders/NationalAssetSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class NationalAssetSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
