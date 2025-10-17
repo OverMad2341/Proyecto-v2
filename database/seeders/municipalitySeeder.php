09d7adb110dd20d3c7a52b71b540c7fc21dd377b
OverMad2341 <eduardocuicas@hotmail.com>
2025-10-17 00:34:57 -0400

    si

--- database/seeders/municipalitySeeder.php
+++ database/seeders/municipalitySeeder.php
@@ -2,33 +2,16 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
 
-class municipalitySeeder extends Seeder
+class MunicipalitySeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        $municipios = [
-            ["name" => "Libertador de Caracas", "federal_state_id" => 1],
-            ["name" => "Chacao", "federal_state_id" => 1],
-            ["name" => "Sucre", "federal_state_id" => 1],
-            ["name" => "Baruta", "federal_state_id" => 1],
-            ["name" => "El Hatillo", "federal_state_id" => 1],
-            
-            ["name" => "Anaco", "federal_state_id" => 2],
-            ["name" => "Aragua de Barcelona", "federal_state_id" => 2],
-            ["name" => "Bolívar", "federal_state_id" => 2],
-            ["name" => "Cachipo", "federal_state_id" => 2],
-            ["name" => "San José de Guanipa", "federal_state_id" => 2],
-        ];
-
-        foreach ($municipios as $municipio) {
-            DB::table('municipality')->insert($municipio);
-        }
+        //
     }
 }
--- database/seeders/parishSeeder.php
+++ database/seeders/parishSeeder.php
@@ -4,20 +4,14 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
-class parishSeeder extends Seeder
+
+class ParishSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        DB::table('parish')->insert([
-            ['name' => 'Parroquia San Juan', 'municipality_id' => 1],
-            ['name' => 'Parroquia El Carmen', 'municipality_id' => 1],
-            ['name' => 'Parroquia San Pedro', 'municipality_id' => 2],
-            ['name' => 'Parroquia La Vega', 'municipality_id' => 2],
-            ['name' => 'Parroquia Santa Rosa', 'municipality_id' => 2],
-        ]);
+        //
     }
 }
